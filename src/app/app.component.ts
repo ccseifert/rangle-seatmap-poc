49f98ae651c85e8bf0daff2eab09f6d616161cba
Unknown <ccseifert@gmail.com>
2018-06-24 20:40:33 -0400

    Adding image detection and basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,31 @@ import { Person } from './person.model';
 })
 export class AppComponent implements OnInit {
   peopleData: Person[];
-  blah = '';
+  imageUrl: string;
+  origImageHeight: number;
+  origImageWidth: number;
 
-  constructor(private peopleService: PeopleService) {}
+  constructor(private peopleService: PeopleService) {
+    this.imageUrl = '/assets/img/map6thFloor.jpg';
+    this.origImageHeight = 0;
+    this.origImageWidth = 0;
+  }
 
   ngOnInit() {
+    var image = new Image();
+    image.addEventListener('load', e => this.handleImageLoad(e));
+    image.src = this.imageUrl;
+    this.origImageHeight = image.height;
+    this.origImageWidth = image.width;
+
     this.peopleService.getData().subscribe(data => {
       this.peopleData = data['people'];
-      console.log(JSON.stringify(this.peopleData));
+      // console.log(JSON.stringify(this.peopleData));
     });
+  }
+
+  handleImageLoad(event): void {
+    this.origImageWidth = event.target.width;
+    this.origImageHeight = event.target.height;
   }
 }
